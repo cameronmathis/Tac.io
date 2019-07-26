2dacb126af4663539d9fa5bdc58b79dc7bc3ebb4
Cameron Mathis <cameronmathis@Camerons-MacBook-Pro.local>
2019-07-25 21:50:39 -0500

    created gameScene and pausePopUp and hotkeys

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,12 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
@@ -23,59 +17,67 @@ public class Main extends Application {
 
     //Variables
     private Stage primaryStage;
-    private SplitPane pane;
-    private Scene scene;
-    private boolean started;
+    private AnchorPane openingPane;
+    private Scene openingScene;
+    private AnchorPane gamePane;
+    private Scene gameScene;
     private boolean paused;
+    private Popup pausedPopUp;
 
     //The GUI interface scene
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         this.primaryStage = primaryStage; //sets this primaryStage as 'the' primaryStage
-        pane = FXMLLoader.load(getClass().getResource("GUI.fxml")); //calls the TitledPane from the fxml file
+        openingPane = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        gamePane = FXMLLoader.load(getClass().getResource("GameScene.fxml"));
 
         /**
          * MAIN STAGE CREATED
          * Main Stage and Scene Created and shown
          */
-        scene = new Scene(pane); //creates a new scene from the TitlesPane created in 'GUI.fxml'
+        openingScene = new Scene(openingPane); //creates a new scene from 'StartScreen.fxml'
+        gameScene = new Scene(gamePane); //created a new scene from 'GameScreen.fxml'
         primaryStage.setTitle("TicTacToe"); //sets the title of the app
-        primaryStage.setScene(scene); //sets the scene on the stage
-        primaryStage.show(); //shows the primaryStage
+        primaryStage.setScene(openingScene); //sets the scene on the stage
         primaryStage.setAlwaysOnTop(false); //set to false to show popups
-        primaryStage.setMinWidth(400); //set the minimum width
-        primaryStage.setMinHeight(400); //set the minimum height
+        primaryStage.setMinWidth(600); //set the minimum width
+        primaryStage.setMinHeight(500); //set the minimum height
         primaryStage.setResizable(true); //makes app able to be resized
+        primaryStage.show(); //shows the primaryStage
 
         /**
          * BUTTON INITIALIZATION
          * Initialize all the buttons
          */
-        Button startBtn = (Button) scene.lookup("#begin"); //calls the browse button from the fxml file
+        Button startBtn = (Button) openingScene.lookup("#start"); //calls the browse button from the fxml file
 
         /**
          * IMAGE LAYER GROUP
          * New group for the image layer
          * (used so you can snapshot the current photo and crop
          */
-        started = false;
         paused = false;
 
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER && !started) {
+        openingScene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
                 start();
             }
-            if (event.getCode() == KeyCode.ENTER && paused) {
+        });
+
+        gameScene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.SPACE && !paused) {
+                pause();
+            } else if (event.getCode() == KeyCode.ENTER && paused) {
                 resume();
             }
         });
 
-        Pane pane = (Pane) scene.lookup("#textPane");
-        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+        Pane pane = (Pane) openingScene.lookup("#textPane");
+        openingScene.widthProperty().addListener((obs, oldVal, newVal) -> {
             pane.setLayoutY(newVal.intValue());
         });
-        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+        openingScene.heightProperty().addListener((obs, oldVal, newVal) -> {
             pane.setLayoutX(newVal.intValue());
         });
 
@@ -91,7 +93,17 @@ public class Main extends Application {
      * Starts the game
      */
     private void start() {
+        primaryStage.setScene(gameScene); //sets the scene on the stage
+        primaryStage.show(); //shows the primaryStage
+    }
 
+    /**
+     * PAUSE METHOD
+     * Pause the game
+     */
+    private void pause() {
+        paused = true;
+        pausedPopUp();
     }
 
     /**
@@ -99,15 +111,12 @@ public class Main extends Application {
      * Starts the game
      */
     private void resume() {
-
-    }
-
-    /**
-     * START METHOD
-     * Starts the game
-     */
-    private void endGame() {
-
+        try {
+            paused = false;
+            pausedPopUp.hide();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     /**
@@ -115,7 +124,7 @@ public class Main extends Application {
      * PopUp for when the game is paused
      */
     private void pausedPopUp() {
-        Popup pausedPopUp = new Popup(); //creates new popup
+        pausedPopUp = new Popup(); //creates new popup
 
         TitledPane pausedPupUpPane = null; //calls popup menu created in 'chooseZipErrorPopUp.fxml' file
 
@@ -130,19 +139,12 @@ public class Main extends Application {
         pausedPopUp.show(primaryStage);
 
         Button resumeBtn = (Button) pausedPupUpPane.lookup("#resume");
-        resumeBtn.setOnAction(browseDismissEvent -> {
-            try {
-                resume();
-                pausedPopUp.hide();
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-        });
+        resumeBtn.setOnAction(browseDismissEvent -> resume());
 
         Button exitBtn = (Button) pausedPupUpPane.lookup("#exit");
-        resumeBtn.setOnAction(browseDismissEvent -> {
+        exitBtn.setOnAction(browseDismissEvent -> {
             try {
-                endGame();
+                System.exit(0);
                 pausedPopUp.hide();
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
